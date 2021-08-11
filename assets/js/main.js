af1dd468c036bd87dc84a19c4143eed24f25c8bb
ElielM9 <elielm291@gmail.com>
2021-08-11 14:19:49 -0500

    este es el prototipo

---
+++ assets/js/main.js
@@ -0,0 +1,89 @@
+const workItems = document.querySelectorAll(".work .item");
+const workItemsCount = workItems.length;
+let currentIndex = 0;
+const closeButton = document.querySelector("#close-button");
+const prevButton = document.querySelector("#prev-button");
+const nextButton = document.querySelector("#next-button");
+const openCloseButton = document.querySelector("#buttonOpenCloseMenu");
+const menuMobileItems = document.querySelector("#menu-mobile-items");
+const typed = new Typed(".typed", {
+  strings:["FRONT-END DEVELOPER"],
+  
+	stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: false, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+openCloseButton.addEventListener("click", (e) => {
+  menuMobileItems.classList.toggle("menu-mobile-closed");
+});
+
+workItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentIndex = parseInt(item.getAttribute("data-id"));
+    const contentArr = document.querySelectorAll("#details-container .item");
+
+    document.querySelectorAll("#details-container .item").forEach((item) => {
+      item.classList.add("item-hide");
+    });
+    document
+      .querySelectorAll("#details-container .item")
+      [currentIndex].classList.toggle("item-hide");
+    document.querySelector("#screen").style["animation-name"] = "fade-in";
+    document.querySelector("body").style["overflow"] = "hidden";
+
+    setTimeout(() => {
+      document.querySelector("#details-container").style.display = "block";
+    }, 1000);
+    setTimeout(() => {
+      document.querySelector("#screen").style = "";
+    }, 1000);
+  });
+});
+
+closeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#screen").style["animation-name"] = "fade-in";
+  document.querySelector("body").style["overflow"] = "auto";
+
+  setTimeout(() => {
+    document.querySelector("#details-container").style.display = "none";
+  }, 1000);
+  setTimeout(() => {
+    document.querySelector("#screen").style = "";
+  }, 1000);
+});
+
+prevButton.addEventListener("click", (e) => {
+  if (currentIndex - 1 < 0) {
+    return;
+  }
+  currentIndex--;
+  loadGalleryItem(currentIndex);
+});
+
+nextButton.addEventListener("click", (e) => {
+  if (currentIndex + 1 == workItemsCount) {
+    return;
+  }
+  currentIndex++;
+  loadGalleryItem(currentIndex);
+});
+
+function loadGalleryItem(index) {
+  const items = document.querySelectorAll("#details-container .item");
+  items.forEach((item) => {
+    item.classList.add("item-hide");
+  });
+  items[index].classList.toggle("item-hide");
+}
